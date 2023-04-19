ce5efaaad43e018b27a1cc2bf70b0092d1e03a5c
Akansha Pardesi <akansha.angel08@gmail.com>
2023-04-19 23:43:04 +0530

    first commit added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Akansha Pardesi
         </a>
       </header>
     </div>
